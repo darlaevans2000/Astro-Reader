9703aef76edb328b6517f439d53f7ec3943d488a
darlaevans2000 <darlaevans2000@gmail.com>
2021-09-22 21:46:22 -0600

    Add header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,12 +4,14 @@ import {
   Switch,
 } from 'react-router-dom';
 import Home from "../Home/Home"
+import Header from "../Header/Header"
 import ReadingPage from '../ReadingPage/ReadingPage';
 import React from 'react';
 
 const App = () => {
   return (
     <div className='app'>
+      <Header />
     <main className='main-section'>
       <Switch>
         <Route exact path="/astro-reader"  render={ () =>  <Home />}/>
