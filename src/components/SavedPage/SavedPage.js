d6bff1f4e733ff169a72a180e2906995062f24d0
darlaevans2000 <darlaevans2000@gmail.com>
2021-09-24 16:04:52 -0600

    Add functionality to dislay saved readings to page

--- src/components/SavedPage/SavedPage.js
+++ src/components/SavedPage/SavedPage.js
@@ -12,17 +12,21 @@ class SavedPage extends Component {
         let values = Object.keys(localStorage);
         return values.forEach((value) => {
         let localStorageObject = localStorage.getItem(`${value}`);
-        this.state.savedReadings.push(localStorageObject)
+        this.state.savedReadings.push(JSON.parse(localStorageObject))
         })
     }
 
-
     render() {
-        return (
-          <div>
-           hi
-          </div>
-        );
+        const readingCards = this.state.savedReadings.map((reading) => {
+        return(
+            <div className="reading-card">
+                Date Saved: {reading.current_date}
+            </div>
+        )
+        });
+        return <div className="saved-area">
+            {readingCards}
+        </div>
     }
 }
 
