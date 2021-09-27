c782703b255671796dc45453c132372f8f809da3
darlaevans2000 <darlaevans2000@gmail.com>
2021-09-26 19:46:20 -0600

    Add remove from storage functionality

--- src/components/SavedCard/SavedCard.js
+++ src/components/SavedCard/SavedCard.js
@@ -1,10 +1,16 @@
-export const SavedCard = ({ card }) => {
+export const SavedCard = ({ card, remove }) => {
   return (
-      <section className='saved-card'>
-          <p>{card.description}</p>
-          <p>{card.current_date}</p>
-      </section>
-    );
-  }
+    <section className="saved-card">
+      <p>{card.description}</p>
+      <p>{card.current_date}</p>
+      <button
+        className="delete"
+        onClick={() => remove(card.lucky_number + card.lucky_time)}
+      >
+        Delete
+      </button>
+    </section>
+  );
+};
 
-  export default SavedCard
+export default SavedCard;
--- src/components/SavedPage/SavedPage.js
+++ src/components/SavedPage/SavedPage.js
@@ -1,36 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import SavedCard from '../SavedCard/SavedCard'
-
+import React, { useState, useEffect } from "react";
+import SavedCard from "../SavedCard/SavedCard";
 
 export const SavedPage = () => {
-const [savedCards, setSavedCards] = useState([])
+  const [savedCards, setSavedCards] = useState([]);
 
-const getFromLocalStorage = () => {
-  const keys = Object.keys(localStorage)
-  let cards;
-  if (keys.length) {
-    cards = keys.map(key => {
-      return JSON.parse(localStorage.getItem(key))
-    })
-    setSavedCards([...cards])
-  }
-}
+  const getFromLocalStorage = () => {
+    const keys = Object.keys(localStorage);
+    let cards;
+    if (keys.length) {
+      cards = keys.map((key) => {
+        return JSON.parse(localStorage.getItem(key));
+      });
+      setSavedCards([...cards]);
+    }
+  };
 
+  const removeFromStorage = (id) => {
+    localStorage.removeItem(id);
+    const remainingSaved = savedCards.filter((card) => id !== card.id);
+    setSavedCards([...remainingSaved]);
+    window.location.reload(false);
+  };
 
-useEffect(() => {
-  getFromLocalStorage()
-}, [])
+  useEffect(() => {
+    getFromLocalStorage();
+  }, []);
 
-
-    
- const cards = savedCards.map((card) => (<SavedCard card={card}/>));
+  const cards = savedCards.map((card) => (
+    <SavedCard card={card} remove={removeFromStorage} />
+  ));
 
   return (
     <>
-    {!cards.length && <h2 className='no-saved'>No readings saved yet.</h2>}
-    <section className='card-container'>{cards}</section>
+      {!cards.length && <h2 className="no-saved">No readings saved yet.</h2>}
+      <section className="card-container">{cards}</section>
     </>
   );
-}
+};
 
-export default SavedPage
+export default SavedPage;
